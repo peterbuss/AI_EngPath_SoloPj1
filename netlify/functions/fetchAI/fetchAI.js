4570489f0d2459007f7200d488f1b9d1ad93c8e5
Peter BUSS <peter.buss@hispeed.ch>
2024-03-15 23:02:10 +0100

    Corrections

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -1,34 +1,31 @@
 import OpenAI from "openai";
-import { Configuration, OpenAIApi } from 'openai';
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
-
-
-
 
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
       const input  = JSON.parse(event.body)
+
+      const openai = new OpenAI({
+        dangerouslyAllowBrowser: true,
+        apiKey: process.env.OPENAI_API_KEY
+      })
+
       const response = await openai.chat.completions.create({
           model: 'gpt-3.5-turbo',
           messages: input
-      });
-      
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ reply: response.data }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      })
+  
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ reply: response }),
+        // // more keys you can return:
+        // headers: { "headerName": "headerValue", ... },
+        // isBase64Encoded: true,
+      }
+
+    } catch (error) {
+      return { statusCode: 500, body: error.toString() }
     }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
 }
 
 module.exports = { handler }
